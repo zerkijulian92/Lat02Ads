308e0728c51e0ce6bd2edd7302a7bcd285ac86b4
zerkijulian92 <zerkijuliandri92@gmail.com>
2020-03-16 21:42:39 +0700

    (Bagian 8) - 5. Membuat Onboarding Kedua Page

---
+++ app/src/main/java/com/takatutustudio/lat02ads/OnboardingTwoActivity.kt
@@ -0,0 +1,12 @@
+package com.takatutustudio.lat02ads
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardingTwoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_two)
+    }
+}
