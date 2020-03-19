25aa0484bec71c00ffb9e8a3988bbe50be0be774
zerkijulian92 <zerkijuliandri92@gmail.com>
2020-03-19 11:51:23 +0700

    (Bagian 8) - 10. Membuat Webview

---
+++ app/src/main/java/com/takatutustudio/lat02ads/BlogActivity.kt
@@ -0,0 +1,19 @@
+package com.takatutustudio.lat02ads
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_blog.*
+
+class BlogActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blog)
+
+        Toast.makeText(this,"Silahkan tunggu...", Toast.LENGTH_SHORT).show()
+
+        web_blog.loadUrl("https://aet.co.id/tentang-kami/")
+        web_blog.settings.javaScriptEnabled
+    }
+}
--- app/src/main/java/com/takatutustudio/lat02ads/HomeActivity.kt
+++ app/src/main/java/com/takatutustudio/lat02ads/HomeActivity.kt
@@ -1,12 +1,22 @@
 package com.takatutustudio.lat02ads
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+//        Berpindah ke BlogActivity
+        tv_blog.setOnClickListener {
+            startActivity(Intent(this, BlogActivity::class.java))
+        }
+        /**
+         * Catt : Jangan lupa tambahkan Internet Permission di Manifest
+         **/
     }
 }
