26c9c19cf4dda6e5f54d9081b8a32fc7167d47e1
zerkijulian92 <zerkijuliandri92@gmail.com>
2020-03-19 15:12:51 +0700

    (Bagian 8) - 11. Finishing

--- app/src/main/java/com/takatutustudio/lat02ads/HomeActivity.kt
+++ app/src/main/java/com/takatutustudio/lat02ads/HomeActivity.kt
@@ -1,7 +1,10 @@
 package com.takatutustudio.lat02ads
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_home.*
 
@@ -11,12 +14,28 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-//        Berpindah ke BlogActivity
+        //Berpindah ke BlogActivity
         tv_blog.setOnClickListener {
             startActivity(Intent(this, BlogActivity::class.java))
         }
         /**
          * Catt : Jangan lupa tambahkan Internet Permission di Manifest
          **/
+    }
+
+    //Menambahkan Fungsi onBackPrassed
+
+    override fun onBackPressed() {
+        AlertDialog.Builder(this)
+            .setTitle("Konfirmasi")
+            .setMessage("Apakah anda ingin keluar dari aplikasi?")
+            .setPositiveButton("Ya", DialogInterface.OnClickListener { dialogInterface, i ->
+                Toast.makeText(this, "Terimakasih telah menggunakan Aplikasi My Financial",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            })
+            .setNegativeButton("Batal", DialogInterface.OnClickListener { dialogInterface, i ->
+
+            }).show()
     }
 }
